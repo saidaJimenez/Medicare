b3cca3ab5afbe3b7b04039b2092448b2d51f7c91
Saida Jiménez Alemán <saidajimenez03@gmail.com>
2023-08-28 23:08:58 +0100

    modificacion

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -12,14 +12,20 @@ const Doctors = () => {
     type="search"
     className='py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer 
     placeholder:text-textColor'
-    placeholder='Buscar  Doctor'
+    placeholder='Busca al Doctor'
     />
     <button className='btn mt-0 rounded-[0px] rounded-r-md'>
       Buscar
     </button>
-
     </div>
     </div>
+  </section>
+  <section>
+  <div className="container">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
+  {doctors.map((doctor) => <DoctorCard key={doctor.id} doctor={doctor}/>)}
+</div>
+      </div>
   </section>
   </>
   
