c1d925f27f80a2dee308780e2fa37ca0f0efde43
Saida Jiménez Alemán <saidajimenez03@gmail.com>
2023-08-28 22:56:09 +0100

    Boton de busqueda

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -8,8 +8,16 @@ const Doctors = () => {
     <div className="container text-center">
     <h2 className='heading'>En cuentra al Doctor que necesitas</h2>
     <div className='max-w-[570px] mt-[30px] mx-auto bg-[#0066ff2c] rounded-md flex items-center justify-between'>
-      <input type="search" className='py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer
-      placeholder:text-textColor'/>
+    <input
+    type="search"
+    className='py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer 
+    placeholder:text-textColor'
+    placeholder='Buscar  Doctor'
+    />
+    <button className='btn mt-0 rounded-[0px] rounded-r-md'>
+      Buscar
+    </button>
+
     </div>
     </div>
   </section>
