3270940b93dd7efb0278165b79498a98d07258b4
Saida Jiménez Alemán <saidajimenez03@gmail.com>
2023-09-08 10:52:27 +0100

    configuracion de paquetes Controllers -auth Router -auth

---
+++ backend/Routes/auth.js
@@ -0,0 +1,4 @@
+import express from 'express'
+import { register,login } from '../Controllers/authController.js'
+
+
---
+++ backend/index.js
@@ -0,0 +1,50 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+
+
+dotenv.config()
+
+
+const app = express()
+
+const port = process.env.PORT || 8000
+
+const corsOptions = {
+    origin: true
+}
+
+
+app.get('/',(req,res) => {
+    res.send('Api trabajando')
+})
+
+//Conección a la base de datos
+mongoose.set('strictQuery',false)
+const connectDB = async() =>{
+    try {
+        await mongoose.connect(process.env.MONGO_URL,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+        })
+
+        console.log("La Base de Datos MongoDB esta conectada ")
+        
+    } catch (error) {
+        console.log("Error de coneccion  ")
+    }
+}
+
+
+//middelware
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors(corsOptions))
+
+app.listen(port, () =>{
+    connectDB()
+    console.log('El servidor esta corrriendo en el puerto ' + port)
+})
