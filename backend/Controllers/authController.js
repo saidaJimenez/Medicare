818d41fdb146b173d176fac22f15783a1a52a77c
Saida Jiménez Alemán <saidajimenez03@gmail.com>
2023-09-09 10:22:12 +0100

    authCotroller auth routes

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -1,10 +1,63 @@
+import User from "../models/UserSchema.js"
+import Doctor from "../models/DoctorSchema.js"
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
 
 
 export const register = async(req,res)=>{
+
+    const{email, password,name,role,photo,genero} = req.body
     try {
-        
+
+
+       let user = null
+
+        if( role ==='paciente'){
+            user = User.findOne({email})
+        } else if(role ==='doctor'){
+            user = Doctor.findOne({email})
+        }
+
+        //Comprobando si el usuario existe
+        if(user){
+            return res.status(400).json({message:'El usuario ya existe'})
+        }
+
+        //password
+
+        const salt = await bcrypt.hash(password,salt)
+
+        const hashPassword = await bcrypt.hash(password,salt)
+
+
+        if(role==='paciente'){
+            user= new User({
+                name,
+                email,
+                password:hashPassword,
+                photo,
+                genero,
+                role
+            })
+        }
+
+        if(role==='doctor'){
+            user= new Doctor({
+                name,
+                email,
+                password:hashPassword,
+                photo,
+                genero,
+                role
+            })
+        }
+
+        await user.save()
+
+        res.status(200).json({success:true,menssage:'Usuario creado con exito'})
+
     } catch (err) {
-        
+        res.status(500).json({success:false,menssage:'Error interno, intentalo de nuevo'})
     }
     
 }
--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser"
 import cors from "cors"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
-
+import authRoute from "./Routes/auth.js"
 
 
 dotenv.config()
@@ -43,6 +43,7 @@ const connectDB = async() =>{
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors(corsOptions))
+app.use('api/v1/auth',authRoute) //dominio/api/v1/auth/register
 
 app.listen(port, () =>{
     connectDB()
